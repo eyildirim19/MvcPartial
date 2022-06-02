f412d9f1acc4e6cb466bc8b34387012691797d3c
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2022-06-02 20:54:59 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcPartial.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PartialResult()
+        {
+            // İlgili klasörde (COntroller bazlı) veya shared folderinde ki view'i partial olarak döner...
+
+
+            // Özellikle ajax (daha sonraki konumuz) reqeustrlerinde resppnse olarak html dönülecekse partialView güzel bir yöntemdir... 
+            return PartialView("_PartialName");
+        }
+    }
+}
